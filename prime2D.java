98e2b531dd47f53e4110befa9034fca39bed9a70
PolimeraLeelakrishna <49052034+PolimeraLeelakrishna@users.noreply.github.com>
2019-04-05 10:23:26 +0530

    update

--- prime2D.java
+++ prime2D.java
@@ -1,13 +1,17 @@
+
+
+
+
+
+
 package com.bridgeit.Data_Structuress;
-
 import com.bridgeit.utility.Utility;
-
 public class prime2D 
 {
  public static void main(String[] args) 
  {
-		{
-
+		       try
+		       {
 			int[] arr = Utility.prime();
 			System.out.print(2 + " ");
 			int[][] arr1 = new int[10][167];
@@ -20,6 +24,12 @@ public class prime2D
 				}
 				System.out.println();
 			}
+		}
+ }
+	catch(Exception e)
+		{
+			System.err.println("invalid");
+			
 		}
     }
 }
