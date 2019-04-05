97c6ef1cd5521516fefb6bcaf7b0a4571be40f01
PolimeraLeelakrishna <49052034+PolimeraLeelakrishna@users.noreply.github.com>
2019-04-05 10:44:03 +0530

    update

--- CalanderQUE.java
+++ CalanderQUE.java
@@ -24,38 +24,38 @@ public class CalanderQUE
     	int y=Integer.parseInt(args[2]);
 	    try
 	    {
-    	if(date[m]==2 && Utility.leapyear(y))
+    	if(date[m]==2 && Utility.leapyear(y))   // month 2 and its leap year than give date to 29
     	{
     		date[m]=29;
     	}
-    	int day=Utility.calday(m, y, d);
-    	int[][] arr=new int[6][7];
-    	int x=1;
+    	int day=Utility.calday(m, y, d);    // calling calday function from utility.
+    	int[][] arr=new int[6][7];          // creating two array object
+    	int x=1;                  // initializing x.
     	for(int i=0;i<6;i++)
     	{
     		for(int j=day; j<7;j++)
     		{
-    			if(date[m]>=x)
+    			if(date[m]>=x) 
     			{
     			arr[i][j]=x;
-    			x++;
+    			x++; // x is increase while month dates greater or equal to x
     		}
     		}
-    		day=0;
+    		day=0;       // intitialize date as zero
   }
-    	   System.out.println(d+"-"+months[m]+"-"+y);
+    	   System.out.println(d+"-"+months[m]+"-"+y);     // print the input date,month,year.
     	   System.out.println(" sun mon tue wed thu fri sat");
     	   for(int i=0;i<6;i++)
     	   {
     		   for(int j=0;j<7;j++)
     		   {
-    			   if(arr[i][j]==0)
+    			   if(arr[i][j]==0)    // if i,j get 0 values keep spaces
     			   {
-    				   System.out.print(" ");
+    				   System.out.print(" "); 
     			   }
     			   if(arr[i][j]!=0){
-    			   System.out.printf("%4d",arr[i][j]);
-    				   que.Enqueue(arr[i][j]);
+    			   System.out.printf("%4d",arr[i][j]);  // print the calander
+    				   que.Enqueue(arr[i][j]);   // send the dates to the que object
     			   } else
     			    System.out.printf(" ");
     			   
